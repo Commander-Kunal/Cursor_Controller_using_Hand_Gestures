a5d398cc0d6ddddc272fee273e7ccbce0fd8544d
Kunal Gupta <kunalg318@gmail.com>
2025-11-02 15:09:41 +0530

    Update AICursorController.py

--- AICursorController.py
+++ AICursorController.py
@@ -4,7 +4,6 @@ import HandTracking as ht
 import time
 from pynput.mouse import Controller, Button
 import pyautogui
-
 ################################
 
 wCam, hCam = 620, 480
@@ -87,20 +86,21 @@ while True:
             elif length > 40:
                 clickFlag = False
         
-        #11. Right Click Mode: Index + Middle + Ring fingers up
-        #if fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 0:
+        # 11. Right Click Mode: Index + Middle + Ring fingers up
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[4] == 0:
             
-            #12. Find distance between fingers
-            #length, img, lineInfo = detector.findDistance(8, 12, img)  # Index to Middle
-            
-            #13. Click if the distance is short
-            #if length < 40 and not clickFlag:
-                #cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 0, 255), cv2.FILLED)
-               # mouse.click(Button.right, 1)
-              #  clickFlag = True
-             #   print("Right Click")
-            #elif length >= 40:
-                #clickFlag = False
+            # 12. Right Click Mode: Middle + Ring finger pinch
+            length, img, lineInfo = detector.findDistance(12, 16, img)  # Middle to Ring
+            if fingers[2] == 1 and fingers[3] == 1:  # Middle and Ring fingers up
+                if length < 40 and not clickFlag:
+                    cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 0, 255), cv2.FILLED)
+                    mouse.click(Button.right, 1)
+                    clickFlag = True
+                    #print("Right Click (Middle + Ring)")
+                elif length >= 40:
+                    clickFlag = False
+
+
 
 
         # 14. Scrolling Gesture: Both index & middle fingers up and moved up/down
@@ -111,12 +111,12 @@ while True:
                 # Scroll Up
             if y2 < hCam // 2 - 30:
                 pyautogui.scroll(40)  # Scroll Up
-                print("Scrolling Up")
+                #print("Scrolling Up")
 
                 # Scroll Down
             elif y2 > hCam // 2 + 30:
                 pyautogui.scroll(-40)  # Scroll Down
-                print("Scrolling Down")
+                #print("Scrolling Down")
             else:
                 scrollFlag = False  # Reset scrolling mode when fingers change
 
@@ -129,12 +129,12 @@ while True:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 100, 255), cv2.FILLED)
                 mouse.press(Button.left)
                 dragFlag = True
-                print("Drag Started") 
+                #print("Drag Started") 
 
             elif length >= 40 and dragFlag:
                 mouse.release(Button.left)
                 dragFlag = False
-                print("Drop")
+                #print("Drop")
 
 
 
